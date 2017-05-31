7439716234719e4f41b18c126096347d0794919c
liu <515046927@qq.com>
2017-05-31 16:32:53 +0800

    000

--- app/src/main/java/com/pi/small/goal/MyApplication.java
+++ app/src/main/java/com/pi/small/goal/MyApplication.java
@@ -5,6 +5,10 @@ import android.app.Application;
 import android.content.Context;
 import android.telephony.TelephonyManager;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+
 import org.xutils.x;
 
 import io.rong.imkit.RongIM;
@@ -18,6 +22,11 @@ public class MyApplication extends Application {
     private TelephonyManager TelephonyMgr;
     public static String deviceId;
 
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -26,6 +35,25 @@ public class MyApplication extends Application {
 
         TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         deviceId = TelephonyMgr.getDeviceId();
+
+
+        mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        //解析定位结果
+
+                    }
+                }
+            }
+        };
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //启动定位
+        mLocationClient.startLocation();
     }
 
     public static String getCurProcessName(Context context) {
