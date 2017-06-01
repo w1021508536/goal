24609b3aa8c214e3bb6e0766d6fefe7905f79a0e
liu <515046927@qq.com>
2017-06-01 09:15:31 +0800

    000

--- app/src/main/java/com/pi/small/goal/MyApplication.java
+++ app/src/main/java/com/pi/small/goal/MyApplication.java
@@ -3,11 +3,15 @@ package com.pi.small.goal;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.pi.small.goal.utils.Utils;
 
 import org.xutils.x;
 
@@ -27,29 +31,70 @@ public class MyApplication extends Application {
     //声明定位回调监听器
     public AMapLocationListener mLocationListener;
 
+    public AMapLocationClientOption mLocationOption;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     public void onCreate() {
         super.onCreate();
         x.Ext.init(this);
         RongIM.init(this);
 
+        sharedPreferences = Utils.UserSharedPreferences(this);
+        editor = sharedPreferences.edit();
+
         TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         deviceId = TelephonyMgr.getDeviceId();
-
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        mLocationClient = new AMapLocationClient(this);
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(600000);
+        //设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//        //启动定位
+//        mLocationClient.startLocation();
 
         mLocationListener = new AMapLocationListener() {
             @Override
             public void onLocationChanged(AMapLocation aMapLocation) {
-                if (aMapLocation != null) {
-                    if (aMapLocation.getErrorCode() == 0) {
-                        //解析定位结果
 
+
+                if (aMapLocation != null) {
+                    //解析定位结果
+                    if (aMapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+                        aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        aMapLocation.getLatitude();//获取纬度
+                        aMapLocation.getLongitude();//获取经度
+                        aMapLocation.getAccuracy();//获取精度信息
+
+
+                        editor.putString("latitude", String.valueOf(aMapLocation.getLatitude()));
+                        editor.putString("longitude", String.valueOf(aMapLocation.getLongitude()));
+
+
+                        editor.commit();
+
+                    } else {
+                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + aMapLocation.getErrorCode() + ", errInfo:"
+                                + aMapLocation.getErrorInfo());
                     }
                 }
             }
         };
-        //初始化定位
-        mLocationClient = new AMapLocationClient(getApplicationContext());
+//        //初始化定位
+//        mLocationClient = new AMapLocationClient(getApplicationContext());
         //设置定位回调监听
         mLocationClient.setLocationListener(mLocationListener);
         //启动定位
--- app/src/main/java/com/pi/small/goal/aim/AimFragment.java
+++ app/src/main/java/com/pi/small/goal/aim/AimFragment.java
@@ -1,22 +1,23 @@
 package com.pi.small.goal.aim;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.pi.small.goal.R;
+import com.pi.small.goal.aim.activity.AddAimActivity;
 
 
-public class AimFragment extends Fragment {
+public class AimFragment extends Fragment implements View.OnClickListener {
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ImageView right_image;
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,9 +26,25 @@ public class AimFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_aim, container, false);
 
 
-
-
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        right_image = (ImageView) view.findViewById(R.id.right_image);
+
+        right_image.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.right_image:
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), AddAimActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
