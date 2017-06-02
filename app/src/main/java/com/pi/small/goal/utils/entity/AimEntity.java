8a6ad330226d861633fe196c7dc8210eb6ec1ccb
liu <515046927@qq.com>
2017-06-02 09:15:55 +0800

    6.1

---
+++ app/src/main/java/com/pi/small/goal/utils/entity/AimEntity.java
@@ -0,0 +1,172 @@
+package com.pi.small.goal.utils.entity;
+
+/**
+ * Created by JS on 2017-06-01.
+ * <p>
+ * 目标
+ */
+
+public class AimEntity {
+
+//    {
+//        "id":25, "name":"刚刚好", "budget":5000, "money":0, "cycle":6, "current":0, "userId":
+//        43, "province":"山东省", "city":"青岛市", "brief":"哈哈", "position":"瘦和美瘦身中心", "longitude":
+//        0.000000, "latitude":0.000000, "support":0, "createTime":1496309464000, "status":1, "img":
+//        "1496309312178.PNG"
+//    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public String getCycle() {
+        return cycle;
+    }
+
+    public void setCycle(String cycle) {
+        this.cycle = cycle;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getSupport() {
+        return support;
+    }
+
+    public void setSupport(String support) {
+        this.support = support;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    private String id;
+    private String name;
+    private String budget;
+    private String money;
+    private String cycle;
+    private String current;
+    private String userId;
+    private String province;
+    private String city;
+    private String brief;
+    private String position;
+    private String longitude;
+    private String latitude;
+    private String support;
+    private String createTime;
+    private String status;
+    private String img;
+
+}
